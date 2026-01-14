4096e6025b4acd814fa234f06c3fce8c014e7e28
Gaetano Aufiero <98214675+AufieroG@users.noreply.github.com>
2026-01-14 12:02:28 +0100

    default cplot configuration change

--- R/default_plot_config.R
+++ R/default_plot_config.R
@@ -63,7 +63,7 @@ default_plot_config <- function() {
     
     pcoa_label = TRUE,             # Draw sample labels using ggrepel
     pcoa_point_size = 3,           # Size of points in ordination plot
-    pcoa_transform = "none",       # "none" | "log" | "hellinger"
+    pcoa_transform = "none",       # "none" | "log"
     pcoa_distance  = "bray",       # "bray" | "euclidean"
     
     # ============================================================
